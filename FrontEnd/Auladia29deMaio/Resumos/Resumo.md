0edbeb28ca0d4b2d59ae4148502533833c4470c2
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2024-05-29 22:28:51 -0300

    React dia 29

---
+++ FrontEnd/Auladia29deMaio/Resumos/Resumo.md
@@ -0,0 +1,11 @@
+# FlexBot
+
+>Caixas flexiveis são utilizadas para montagem do layout de paginas HTML de maneira responsiva e auto organizavel
+>Com caixa flexiveis é possivel prever e determinar que os elementos jamais sairão da posição onde foram construidos de maneira desordenada.
+
+>Todo elemento flexivel, parte de um conteiner ou seja uma DIV.
+>Quem determinara que o conteiner criado sera flexivel sempre será o CSS
+
+## Flex-Direction
+
+> Muda a ordem das divis
---
+++ FrontEnd/Auladia29deMaio/app2/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="flex-conteiner">
+        <div className='flex1'></div>
+        <div className='flex2'>
+        <div className='flex4'></div>
+        </div>
+        <div className='flex3'></div>
+      </div>
+      <div className="flex-conteiner1">
+        <div className='flex-1'></div>
+      </div>
+      <div className="flex-conteiner2">
+        <div className='flexA1'></div>
+        <div className='flexA2'></div>
+        <div className='flexA3'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
