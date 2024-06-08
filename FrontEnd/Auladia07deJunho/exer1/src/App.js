b0479e6e39d4e16e9f2e50b075613e7b45611dcb
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2024-06-07 22:22:38 -0300

    Atividade de Alinhamento e Justificativa

---
+++ FrontEnd/Auladia07deJunho/exer1/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className = 'container'>
+      <div className ='flex1'></div>
+      <div className ='flex2'></div>
+      <div className ='flex3'>
+        <div className = 'flex3-1'></div>
+        <div className = 'flex3-2'></div>
+      </div>
+      <div className ='flex4'>
+        <div className = 'flex4-1'></div>
+        <div className ='flex4-2'></div>
+      </div>
+      <div className ='flex5'></div>
+      <div className ='flex6'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
