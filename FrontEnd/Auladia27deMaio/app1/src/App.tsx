4e8af645bb8daa19fe2edbfddd3a210d656e6404
Lucas Lima <lucasmslima1@gmail.com>
2024-07-12 02:24:34 -0300

    OK

--- FrontEnd/Auladia27deMaio/app1/src/App.tsx
+++ FrontEnd/Auladia27deMaio/app1/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+        
+        <div className="App">
      <header>
         <h1>Lucas Lima</h1>
         <p>Análise e Desenvolvimento de Sistemas</p>
--- FrontEnd/Auladia29deMaio/app2/src/App.tsx
+++ FrontEnd/Auladia29deMaio/app2/src/App.tsx
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <div className="flex-conteiner">
-        <div className='flex1'></div>
-        <div className='flex2'>
-        <div className='flex4'></div>
-        </div>
-        <div className='flex3'></div>
-      </div>
-      <div className="flex-conteiner1">
-        <div className='flex-1'></div>
-      </div>
-      <div className="flex-conteiner2">
-        <div className='flexA1'></div>
-        <div className='flexA2'></div>
-        <div className='flexA3'></div>
-      </div>
+    <div className="App">
+      <nav className="navbar">
+
+        <a href="#home" className="navbar-brand"><img src={`${process.env.PUBLIC_URL}/Logo-Vetor-01.png`} alt="Logo" className="logo" /></a>
+        <ul className="nav-items">
+          <li className="nav-item">
+            <a href="#vagas" className="nav-link">Vagas</a>
+          </li>
+          <li className="nav-item">
+            <a href="#carreiras" className="nav-link">Carreiras</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a href="#por-que-nos" className="nav-link">Por que Nós?</a>
+            <div className="dropdown-menu">
+              <a href="#para-empresa" className="dropdown-item">Para Empresa</a>
+              <a href="#para-candidatos" className="dropdown-item">Para Candidatos</a>
+              <a href="#quem-somos" className="dropdown-item">Quem Somos</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
