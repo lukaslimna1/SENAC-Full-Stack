2ccbce6babbf23c3efd6a36a203d4271d99422e0
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2024-07-12 22:15:06 -0300

    0001
    melhor revisão

--- FrontEnd/Revisao/revisao/src/App.tsx
+++ FrontEnd/Revisao/revisao/src/App.tsx
@@ -3,9 +3,13 @@ import {Routes, Route} from 'react-router-dom'
 /*Paginas*/
 import Home from './pagina/home';
 import QuemSomos from './pagina/quemsomos';
+import Professores from './pagina/professores';
+import Alunos from './pagina/alunos';
 
 /*CSS*/
 import './estilos/estilo.css';
+import AlunoDetalhe from './pagina/alunoDetalhe';
+
 
 function App() {
   return (
@@ -13,6 +17,9 @@ function App() {
       <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/quemsomos' element={<QuemSomos/>} />
+      <Route path='/professores' element={<Professores/>} />
+      <Route path='/alunos' element={<Alunos/>} />
+      <Route path='/alunos/:aluno' element={<AlunoDetalhe/>} />
       </Routes>
     </div>
   );
---
+++ FrontEnd/Revisao/revisao/src/pagina/professores.tsx
@@ -0,0 +1,15 @@
+import Cabecalho from '../componentes/cabecalho'
+import '../estilos/estilo.css'
+
+function Professores() {
+    return (
+        <div>
+            <Cabecalho />
+            Pagina sobre os professores do Senac
+
+        </div>
+    )
+    
+    
+}
+export default Professores
