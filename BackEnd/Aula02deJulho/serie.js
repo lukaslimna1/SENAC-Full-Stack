09dac589ab46be3ec14188378977091624c8fc09
Lucas Lima <lucasmslima1@gmail.com>
2024-07-18 03:48:09 -0300

    00001

--- BackEnd/Aula02deJulho/serie.js
+++ BackEnd/Aula02deJulho/serie.js
@@ -1,14 +1,18 @@
-import filme from "./filme";
+import Filme from "./filme.js"; // Importando a classe Filme
 
-export	class Serie extends filme {
-    constructor(nome, nomeSerie, duracao, sinopse, ano, episodio, temporada){
-        super(nome, duracao, sinopse, ano)
+export default class Serie extends Filme {
+  constructor(nome, duracao, sinopse, ano, nomeSerie, episodio, temporada) {
+    super(nome, duracao, sinopse, ano);
+    this.nomeSerie = nomeSerie;
+    this.episodio = episodio;
+    this.temporada = temporada;
+  }
 
-        this.nomeSerie = nomeSerie
-        this.episodio = episodio
-        this.temporada = temporada
-    }
+  assistir() {
+    return `Assistindo à série ${this.nomeSerie}, temporada ${this.temporada}, episódio ${this.episodio}. Tempo restante -> ${this.duracao} minutos`;
+  }
 
-   
-    
+  compartilhar() {
+    return `Estou assistindo à série ${this.nomeSerie}, temporada ${this.temporada}. Sinopse: ${this.sinopse}. Foi lançada em ${this.ano}.`;
+  }
 }
